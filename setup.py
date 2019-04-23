8d9caedf009cf7ad69c7e030b3e3d8d8e2ac5fe4
Samuel Heinrichs <ti.samuelh@gmail.com>
2019-04-23 13:06:43 -0300

    package publishing

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ except:
 
 setuptools.setup(
     name="pubsub_queue",
-    version="0.0.1",
+    version="0.0.5",
     author="Samuel Heinrichs",
     author_email="samuel@n2bbrasil.com",
     description="PubSub Queue Manager",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    include_package_data=True,
     url="https://github.com/N2BBrasil/pubsub-queue",
-    install_requires=[r.split('==')[0] for r in open("requirements.txt").read().split("\n")],
+    install_requires=[r for r in open("requirements.txt").read().split("\n")],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
